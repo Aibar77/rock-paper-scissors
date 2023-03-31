1309a3b2cecc15d1595161e4d84fa51d47dfe0ef
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-03-31 21:37:19 +0600

    Create basic game with js

---
+++ game.js
@@ -0,0 +1,83 @@
+function getComputerChoice() {
+  const choices = ['Rock','Paper','Scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(player,computer) {
+  let psel = player[0].toUpperCase()+player.slice(1).toLowerCase();
+  
+  if (psel === computer) {
+
+    console.log('You tied');
+    console.log(`Current Score: ${userScore}:${compScore}`)
+
+
+  } else if (psel === 'Rock' && computer === 'Paper' || psel === 'Paper' && computer === 'Scissors' || psel === 'Scissors' && computer === 'Rock') {
+    compScore++;
+    console.log(`You lose! ${computer} beats ${psel}`);
+    console.log(`Current Score: ${userScore}:${compScore}`)
+
+  } else {
+    userScore++;
+    console.log(`You win! ${psel} beats ${computer}`);
+    console.log(`Current Score: ${userScore}:${compScore}`)
+
+  }
+}
+let userScore = 0;
+let compScore = 0;
+function game() {
+  let numberOfRounds = prompt('How much rounds do you want?');
+  if (numberOfRounds == null || numberOfRounds == '') {
+    console.log('You choose exit the game! Reload to restart!');
+    return;
+  }
+  numberOfRounds = parseInt(numberOfRounds);
+  if (numberOfRounds<= 100) {
+    
+    
+    console.log('The game is started!!!')
+    for (let i = 0; i< numberOfRounds;i++) {
+      let userSelection = prompt('Choose you Beat!');
+      if (userSelection == null) {
+        console.log('Exit the Game!')
+        return alert('You exit the Game! Reload to start again!');
+      }
+      userSelection = userSelection.toLowerCase();
+      let computerSelection = getComputerChoice();
+
+      if (userSelection == 'rock' || userSelection == 'paper' || userSelection == 'scissors') {
+        playRound(userSelection,computerSelection);
+        
+      } else {
+        console.log('Incorrect choose! Do you really understand how to play this game?')
+        userScore = 0;
+        compScore = 0;
+        return game()
+      }
+     
+    }
+    console.log('The game is end!!!')
+    if (userScore == compScore) {
+      console.log(`It's a Draw! Total Score: ${userScore}:${compScore}`)
+    } else if (userScore < compScore) {
+      console.log(`You lose bro! Reload and try again! Total Score: ${userScore}:${compScore}`)
+    } else {
+      console.log(`You win! My Congratulations! Total Score: ${userScore}:${compScore}`)
+    }
+    
+  } else if (numberOfRounds > 100){
+    alert('This is too much bro! Pick again!')
+    game();
+  }  else if (isNaN(numberOfRounds)) {
+    console.log(numberOfRounds)
+    alert('This is not a number bro! Pick again')
+    game()
+  } 
+  
+}
+game()
+
+
+
+
